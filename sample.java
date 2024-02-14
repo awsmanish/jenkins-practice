2ac75e19529d461ad0d6f4d08f0c0c3176c5b6d5
awsmanish <139372084+awsmanish@users.noreply.github.com>
2024-02-14 17:31:19 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 class simple{
 public static void main(string args[]){
 
-System.out.println("welcome to cloud word add new thinks");
+System.out.println("welcome to cloud word add new this");
 }
 }
